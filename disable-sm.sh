c8be29ad9af545af018575a2969b751234299d85
redsmitty <richard.ch.smith@gmail.com>
2020-05-18 20:04:16 -0500

    Update disable-sm.sh

--- disable-sm.sh
+++ disable-sm.sh
@@ -1,3 +1,6 @@
+# Download this file to your local system and run it as the root user.
+# Or you can copy and paste it into a file on your local system and run it as the root user.
+
 sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/subscription-manager.conf
 sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/product-id.conf
 chkconfig rhsmcertd off
